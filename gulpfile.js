e1620103874d6556983c052df62bc8bccb20ced6
Ian Benedict <ianbenedict@gmail.com>
2014-08-27 17:43:44 -0400

    Added a couple of gulp plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var sass = require('gulp-sass');
+var minify_css = require('gulp-minify-css');
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function () {
+gulp.task('clean', function() {
+  return gulp.src('public', { read: false })
+             .pipe(clean());
+});
+
+gulp.task('sass', ['clean'], function () {
   gulp.src('private/scss/*.scss')
       .pipe(sass())
+      .pipe(minify_css())
       .pipe(gulp.dest('public/css'));
 });
 
