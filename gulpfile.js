705a45fe1874ea6001666a21e321b27f88d5af2d
Ian Benedict <ianbenedict@gmail.com>
2014-08-27 15:43:00 -0400

    Tweaked bower.json and added uglify to script building

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
-var gulp = require('gulp');
-    clean = require('gulp-clean'),
+var del = require('del'),
+    gulp = require('gulp');
     sass = require('gulp-sass'),
     minify_css = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    streamify = require('gulp-streamify'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream');
 
 gulp.task('default', ['styles', 'scripts']);
 
-gulp.task('clean', function() {
-  return gulp.src('./public', { read: false })
-             .pipe(clean());
+gulp.task('clean', function (callback) {
+  del('public', callback);
 });
 
 gulp.task('styles', ['clean'], function () {
-  return gulp.src('./private/styles/*.scss')
+  return gulp.src('private/styles/*.scss')
          .pipe(sass())
          .pipe(minify_css())
-         .pipe(gulp.dest('./public/styles'));
+         .pipe(gulp.dest('public/styles'));
 });
 
 gulp.task('scripts', ['clean'], function () {
   return browserify('./private/scripts/main.js', { debug: true })
          .bundle()
          .pipe(source('main.js'))
-         .pipe(gulp.dest('./public/scripts'));
+         .pipe(streamify(uglify()))
+         .pipe(gulp.dest('public/scripts'));
 });
