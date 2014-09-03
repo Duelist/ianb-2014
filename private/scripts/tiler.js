309282325c49137d77fffdffb9cfabfdc8d72481
Ian Benedict <ianbenedict@gmail.com>
2014-09-03 08:05:35 -0400

    Added tile rendering to index page.

--- private/scripts/tiler.js
+++ private/scripts/tiler.js
@@ -11,21 +11,32 @@ module.exports = (function () {
         success: function (data) {
           var json_data = data;
           feed = json_data;
+          render();
         }
       });
     }
+
+    function render_tile(post) {
+      var tile = $('<div>')
+                 .addClass('tile'),
+          tile_content = $('<div>')
+                         .addClass('tile-content');
+      tile_content.html(post.messages.join(' '));
+      tile.append(tile_content);
+      return tile;
+    }
   
     function render() {
-      var rendered_tiles = [],
-          tile = $('<div>');
-      console.log(feed[0]);
-      rendered_tiles.push(tile); 
-      console.log(rendered_tiles);
+      var rendered_tiles = [];
+      console.log(feed);
+      rendered_tiles = feed.map(function (post) {
+        return render_tile(post);
+      });
+      $('.tiled-feed').append(rendered_tiles);
     }
   
     return {
-      init: init,
-      render: render
+      init: init
     };
   }());
 }());
